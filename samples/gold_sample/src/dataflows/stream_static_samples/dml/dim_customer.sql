91ddf371c67804c8099ac8e5b3f5d955fdcf8217
Haille W <haille.woldegebriel@databricks.com>
2026-01-07 08:11:47 +1100

    fix sequence by bug in gold samples

--- samples/gold_sample/src/dataflows/stream_static_samples/dml/dim_customer.sql
+++ samples/gold_sample/src/dataflows/stream_static_samples/dml/dim_customer.sql
@@ -6,8 +6,8 @@ SELECT
     ca.CITY,
     ca.STATE,
     c.DELETE_FLAG,
-    drv.__START_AT
+    drv.__START_AT AS SEQUENCE_BY_COLUMN
 FROM
     STREAM(live.v_final_cdf_feed) AS drv
-    JOIN {bronze_schema}.customer AS c ON drv.CUSTOMER_ID = c.CUSTOMER_ID
-    JOIN {bronze_schema}.customer_address AS ca ON drv.CUSTOMER_ID = ca.CUSTOMER_ID
+    LEFT JOIN {bronze_schema}.customer AS c ON drv.CUSTOMER_ID = c.CUSTOMER_ID
+    LEFT JOIN {bronze_schema}.customer_address AS ca ON drv.CUSTOMER_ID = ca.CUSTOMER_ID
